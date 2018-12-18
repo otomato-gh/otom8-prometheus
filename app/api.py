1a9c14ec49f1d3e857c3e84f0ef1f4a4595a1a26
Ant Weiss <anton@otomato.link>
2018-12-18 09:38:16 -0600

    api.py - fix logging

--- app/api.py
+++ app/api.py
@@ -1,25 +1,24 @@
 import time
+import logging
 from eve import Eve
 from eve_swagger import swagger, add_documentation
-#from flask_prometheus import monitor
 from prometheus_client import start_http_server
 
 app = Eve()
 app.register_blueprint(swagger)
 
 def pre_users_get_callback(request, lookup):
-     print('A GET request on the users endpoint has just been received!')
-def pre_users_post_callback(request):
-    print('A POST request on the users endpoint has just been received')
+     app.logger.info('A GET request on the users endpoint has just been received!')
+def pre_users_post_callback(request): 
+     app.logger.info('A POST request on the users endpoint has just been received')
 
 app.on_pre_GET_users += pre_users_get_callback
 app.on_pre_POST_users += pre_users_post_callback
 
 def post_users_get_callback(request, payload):
-    print('A GET request on the users endpoint has just been performed!')
-         
+    app.logger.info('A GET request on the users endpoint has just been performed!')
 def post_users_post_callback(request, payload):
-    print('A POST request on the users endpoint has just been performed')
+    app.logger.info('A POST request on the users endpoint has just been performed')
                      
 app.on_post_GET_users += post_users_get_callback
 app.on_post_POST_users += post_users_post_callback
@@ -52,6 +51,7 @@ def healthz():
 def version():
     return app.config['SWAGGER_INFO']['version']
 
+
 @app.route('/ping')
 def ping():
     if not hasattr(ping, "latency"):
@@ -59,8 +59,9 @@ def ping():
     time.sleep(ping.latency)
     if ping.latency > 0:
         ping.latency -= 1
-    return 'Pong ' + str(ping.latency)
+    return 'Pong ' + str(ping.latency + 1)
 
 if __name__ == '__main__':
+	app.logger.setLevel(logging.INFO)
 	start_http_server(8000)
 	app.run(host='0.0.0.0')
